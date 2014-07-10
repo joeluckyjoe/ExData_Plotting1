af72c8e46970491f809e911d937f4c639887b068
Giuseppe Albanese <joeluckyjoe@hotmail.com>
2014-07-10 22:20:39 +0200

    plot3.R plot3.png plot4.R plot4.png

---
+++ plot3.R
@@ -0,0 +1,76 @@
+##  download the zip file to your current working directory
+
+setwd("C:/Users/P06226/datascienceeda/ExData_Plotting1")
+
+zipUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+
+if (!file.exists("./household_power_consumption.zip")) {
+        download.file(zipUrl, destfile = "./household_power_consumption.zip",
+                      method = "internal", mode="wb")
+}
+
+
+## get the complete data
+
+household_power_consumptionunz <- unz("./household_power_consumption.zip",
+                                      "household_power_consumption.txt")
+
+household_power_consumption <- read.csv(household_power_consumptionunz, header = TRUE,
+                                        sep = ";", 
+                                        colClasses = c("character", "character", "numeric", 
+                                                       "numeric", "numeric", "numeric",
+                                                       "numeric", "numeric", "numeric"),
+                                        na.strings = "?") 
+
+
+## convert Time column using strptime()
+
+household_power_consumption$Time <- strptime(paste(household_power_consumption$Date, 
+                                                   household_power_consumption$Time),
+                                             format = "%d/%m/%Y %H:%M:%S")
+
+
+## convert Date column using as.date()
+
+household_power_consumption$Date <- as.Date(household_power_consumption$Date, format = "%d/%m/%Y")
+
+## subset the data from the dates 2007-02-01 and 2007-02-02
+
+subset <- household_power_consumption[which(
+        household_power_consumption$Date == "2007-02-01" |
+                household_power_consumption$Date == "2007-02-02"),]
+
+## open png device
+
+png(filename = "plot3.png")
+
+## plot the Sub_metering_1 data with appropriate label
+
+Sys.setlocale("LC_TIME", "English") ## change locale to English
+
+plot(subset$Time, subset$Sub_metering_1,
+     ylab = "Energy sub metering",
+     xlab = "",
+     type="l")
+
+## add the Sub_metering_2 data
+
+lines(subset$Time, subset$Sub_metering_2,
+       col = "red")
+
+## add the Sub_metering_3 data
+
+lines(subset$Time, subset$Sub_metering_3,
+       col = "blue")
+
+## add legends
+
+legend("topright",
+       col = c("black", "red", "blue"),
+       legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),
+       lty = c(1, 1, 1))
+
+## close the file device
+
+dev.off()
+
---
+++ plot4.R
@@ -0,0 +1,102 @@
+##  download the zip file to your current working directory
+
+setwd("C:/Users/P06226/datascienceeda/ExData_Plotting1")
+
+zipUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+
+if (!file.exists("./household_power_consumption.zip")) {
+        download.file(zipUrl, destfile = "./household_power_consumption.zip",
+                      method = "internal", mode="wb")
+}
+
+
+## get the complete data
+
+household_power_consumptionunz <- unz("./household_power_consumption.zip",
+                                      "household_power_consumption.txt")
+
+household_power_consumption <- read.csv(household_power_consumptionunz, header = TRUE,
+                                        sep = ";", 
+                                        colClasses = c("character", "character", "numeric", 
+                                                       "numeric", "numeric", "numeric",
+                                                       "numeric", "numeric", "numeric"),
+                                        na.strings = "?") 
+
+
+## create a datetime  column using strptime()
+
+household_power_consumption$datetime <- strptime(paste(household_power_consumption$Date,
+                                                       household_power_consumption$Time),
+                                                 format = "%d/%m/%Y %H:%M:%S")
+
+
+## convert Date column using as.date()
+
+##household_power_consumption$Date <- as.Date(household_power_consumption$Date, format = "%d/%m/%Y")
+
+## subset the data from the dates 2007-02-01 and 2007-02-02
+
+subset <- household_power_consumption[which(
+        as.Date(household_power_consumption$Date, format = "%d/%m/%Y") == "2007-02-01" |
+                as.Date(household_power_consumption$Date, format = "%d/%m/%Y") == "2007-02-02"),]
+
+## open png device
+
+png(filename = "plot4.png")
+
+Sys.setlocale("LC_TIME", "English") ## change locale to English
+
+## change par() in order to get multiple base plots
+
+par(mfcol = c(2,2))
+
+
+with(subset, {
+
+## plot Global_active_power
+
+plot(datetime, Global_active_power,
+     ylab = "Global Active Power",
+     xlab = "",
+     type="l")
+
+## plot the Sub_metering_1 data with appropriate label
+
+plot(datetime, Sub_metering_1,
+     ylab = "Energy sub metering",
+     xlab = "",
+     type="l")
+
+## add the Sub_metering_2 data
+
+lines(datetime, Sub_metering_2,
+      col = "red")
+
+## add the Sub_metering_3 data
+
+lines(datetime, Sub_metering_3,
+      col = "blue")
+
+## add legends
+
+legend("topright",
+       bty = "n",
+       col = c("black", "red", "blue"),
+       legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),
+       lty = c(1, 1, 1))
+
+## plot Voltage
+
+plot(datetime, Voltage,
+     type="l")
+
+## plot Global_reactive_power
+
+plot(datetime, Global_reactive_power,
+     type="l")
+
+})
+
+## close the file device
+
+dev.off()
