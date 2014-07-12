943d417f2300fbdacb2c7cbad203b64529f67cea
Giuseppe Albanese <joeluckyjoe@hotmail.com>
2014-07-12 20:46:02 +0200

    correction to plot4.R file will be downloaded to working directory

--- plot4.R
+++ plot4.R
@@ -1,6 +1,11 @@
-##  download the zip file to your current working directory
+## plot4.R will create plot4.png which corresponds to plot4 of the Course Project 1 of the
+## Exploratory Data Analysis course.
+## It will first download household_power_consumption.zip to the current working directory
+## if it does not already exist.
+## It will also subset the data from the dates 2007-02-01 and 2007-02-02.
+## Finaly, it will  plot multiple base plots.
 
-setwd("C:/Users/P06226/datascienceeda/ExData_Plotting1")
+##  download the zip file to your current working directory
 
 zipUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
 
@@ -29,11 +34,6 @@ household_power_consumption$datetime <- strptime(paste(household_power_consumpti
                                                        household_power_consumption$Time),
                                                  format = "%d/%m/%Y %H:%M:%S")
 
-
-## convert Date column using as.date()
-
-##household_power_consumption$Date <- as.Date(household_power_consumption$Date, format = "%d/%m/%Y")
-
 ## subset the data from the dates 2007-02-01 and 2007-02-02
 
 subset <- household_power_consumption[which(
@@ -52,50 +52,50 @@ par(mfcol = c(2,2))
 
 
 with(subset, {
-
-## plot Global_active_power
-
-plot(datetime, Global_active_power,
-     ylab = "Global Active Power",
-     xlab = "",
-     type="l")
-
-## plot the Sub_metering_1 data with appropriate label
-
-plot(datetime, Sub_metering_1,
-     ylab = "Energy sub metering",
-     xlab = "",
-     type="l")
-
-## add the Sub_metering_2 data
-
-lines(datetime, Sub_metering_2,
-      col = "red")
-
-## add the Sub_metering_3 data
-
-lines(datetime, Sub_metering_3,
-      col = "blue")
-
-## add legends
-
-legend("topright",
-       bty = "n",
-       col = c("black", "red", "blue"),
-       legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),
-       lty = c(1, 1, 1))
-
-## plot Voltage
-
-plot(datetime, Voltage,
-     type="l")
-
-## plot Global_reactive_power
-
-plot(datetime, Global_reactive_power,
-     type="l")
-
-})
+        
+        ## plot Global_active_power
+        
+        plot(datetime, Global_active_power,
+             ylab = "Global Active Power",
+             xlab = "",
+             type="l")
+        
+        ## plot the Sub_metering_1 data with appropriate label
+        
+        plot(datetime, Sub_metering_1,
+             ylab = "Energy sub metering",
+             xlab = "",
+             type="l")
+        
+        ## add the Sub_metering_2 data
+        
+        lines(datetime, Sub_metering_2,
+              col = "red")
+        
+        ## add the Sub_metering_3 data
+        
+        lines(datetime, Sub_metering_3,
+              col = "blue")
+        
+        ## add legends
+        
+        legend("topright",
+               bty = "n",
+               col = c("black", "red", "blue"),
+               legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),
+               lty = c(1, 1, 1))
+        
+        ## plot Voltage
+        
+        plot(datetime, Voltage,
+             type="l")
+        
+        ## plot Global_reactive_power
+        
+        plot(datetime, Global_reactive_power,
+             type="l")
+        
+        })
 
 ## close the file device
 
